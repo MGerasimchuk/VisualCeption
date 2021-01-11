800369a9cf089275e536a197d3cd59eb799556ff
Mikhail Gerasimchuk <mgerasimchuk@pushwoosh.com>
2021-01-12 00:02:46 +0700

    fix for myself

--- module/VisualCeption.php
+++ module/VisualCeption.php
@@ -375,6 +375,8 @@ class VisualCeption extends CodeceptionModule
         $elementPath = $this->getScreenshotPath($identifier);
         $screenShotImage = new \Imagick();
 
+        $this->hideElementsForScreenshot($excludeElements);
+
         if ($this->config["fullScreenShot"] == true) {
             $height = $this->webDriver->executeScript("var ele=document.querySelector('html'); return ele.scrollHeight;");
             $viewportHeight = $this->webDriver->executeScript("return window.innerHeight");
@@ -397,14 +399,14 @@ class VisualCeption extends CodeceptionModule
             $fullShot->writeImage($elementPath);
 
         } else {
-            $this->hideElementsForScreenshot($excludeElements);
             $screenshotBinary = $this->webDriver->takeScreenshot();
-            $this->resetHideElementsForScreenshot($excludeElements);
 
             $screenShotImage->readimageblob($screenshotBinary);
             $screenShotImage->cropImage($coords['width'], $coords['height'], $coords['offset_x'], $coords['offset_y']);
             $screenShotImage->writeImage($elementPath);
         }
+
+        $this->resetHideElementsForScreenshot($excludeElements);
 
         return $elementPath;
     }
